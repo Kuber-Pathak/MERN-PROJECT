104833ced67ce4bd9d776fab27e9912daa209a3e
Kuber-Pathak <kuberpathak124@gmail.com>
2024-07-28 15:34:38 +0545

    authentication

--- e-mart/db.connect.js
+++ e-mart/db.connect.js
@@ -1,4 +1,4 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 
 const dbUserName = process.env.DB_USER_NAME;
 const dbPassword = encodeURIComponent(process.env.DB_PASSWORD);
